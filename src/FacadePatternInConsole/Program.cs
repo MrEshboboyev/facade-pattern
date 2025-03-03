549302a9394cd18521ec0259b31d300c96a36ab7
MrEshbobotev <mreshboboyev@gmail.com>
2025-03-03 12:42:44 +0500

    Facade Pattern  applied in Console

---
+++ src/FacadePatternInConsole/Program.cs
@@ -0,0 +1,10 @@
+﻿// The client code may have some of the subsystem's objects already created.
+// In this case, it might be worthwhile to initialize the Facade with these objects instead of
+// letting the Facade create new instances.
+using FacadePatternInConsole;
+using FacadePatternInConsole.Subsystems;
+
+Subsystem1 subsystem1 = new Subsystem1();
+Subsystem2 subsystem2 = new Subsystem2();
+Facade facade = new Facade(subsystem1, subsystem2);
+Client.ClientCode(facade);
